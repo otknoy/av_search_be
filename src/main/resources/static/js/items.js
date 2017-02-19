5d5b2591f4fadb0df6ec4bdbc194c41238a94eb4
Naoya Otsuka <otknoy@gmail.com>
2017-02-19 13:15:07 +0900

    Modify template

---
+++ src/main/resources/static/js/items.js
@@ -0,0 +1,3 @@
+riot.tag2('items', '<div each="{item in items}"> <img riot-src="{item.imageUrl}"> <p>{item.title}</p> <div each="{actress in item.actress}"> <p> <a href="?query={actress}">{actress}</a> </p> </div> <div each="{genre in item.genre}"> <p> <a href="?query={genre}">{genre}</a> </p> </div> </div>', '', '', function(opts) {
+   this.items = opts.items;
+});
