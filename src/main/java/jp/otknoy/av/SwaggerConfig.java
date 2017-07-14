208b711fde1a88af2bf5e14aad6f5439a3de00d4
otknoy <otknoy@gmail.com>
2017-07-14 23:39:58 +0900

    Swagger

---
+++ src/main/java/jp/otknoy/av/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package jp.otknoy.av;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+public class SwaggerConfig {
+
+    @Bean
+    public Docket documentation() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(regex("^/(?!error).*$"))
+                .build()
+                .pathMapping("/")
+                .apiInfo(metadata());
+    }
+
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfiguration.DEFAULT;
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder().title("Employee API").version("1.0").build();
+    }
+}
