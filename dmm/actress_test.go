ba9ffc876724be0f3f9cc3cc26a1cc70d848854b
Naoya Otsuka <otknoy@gmail.com>
2017-10-17 15:21:43 +0900

    Add test case

--- dmm/actress_test.go
+++ dmm/actress_test.go
@@ -2,6 +2,8 @@ package dmm
 
 import (
 	"testing"
+
+	"encoding/json"
 )
 
 func TestUnmarshalJSON(t *testing.T) {
@@ -57,4 +59,62 @@ func TestUnmarshalJSON(t *testing.T) {
 			t.Fatal("failed test")
 		}
 	}
+}
+
+func TestChunkArray(t *testing.T) {
+	b := []byte(`[
+{
+"id": 1234,
+"name": "foo"
+},
+{
+"id": "1234",
+"name": "foo_bar"
+},
+{
+"id": 5678,
+"name": "foo"
+},
+{
+"id": "5678",
+"name": "foo_bar"
+}
+]`)
+
+	ary := []interface{}{}
+	err := json.Unmarshal(b, &ary)
+	if err != nil {
+		t.Fatal("failed to unmarshal test data")
+	}
+
+	chunks := chunkArray(ary, 2)
+
+	if ary[0].(map[string]interface{})["id"] != chunks[0][0].(map[string]interface{})["id"] {
+		t.Fatal("fail test")
+	}
+	if ary[0].(map[string]interface{})["name"] != chunks[0][0].(map[string]interface{})["name"] {
+		t.Fatal("fail test")
+	}
+
+	if ary[1].(map[string]interface{})["id"] != chunks[0][1].(map[string]interface{})["id"] {
+		t.Fatal("fail test")
+	}
+	if ary[1].(map[string]interface{})["name"] != chunks[0][1].(map[string]interface{})["name"] {
+		t.Fatal("fail test")
+	}
+
+	if ary[2].(map[string]interface{})["id"] != chunks[1][0].(map[string]interface{})["id"] {
+		t.Fatal("fail test")
+	}
+	if ary[2].(map[string]interface{})["name"] != chunks[1][0].(map[string]interface{})["name"] {
+		t.Fatal("fail test")
+	}
+
+	if ary[3].(map[string]interface{})["id"] != chunks[1][1].(map[string]interface{})["id"] {
+		t.Fatal("fail test")
+	}
+	if ary[3].(map[string]interface{})["name"] != chunks[1][1].(map[string]interface{})["name"] {
+		t.Fatal("fail test")
+	}
+
 }
