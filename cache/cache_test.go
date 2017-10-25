6f71196643fba7298f80ac9566ccf4c66657072c
Naoya Otsuka <otknoy@gmail.com>
2017-10-25 22:16:24 +0900

    Use go-cache

---
+++ cache/cache_test.go
@@ -0,0 +1,37 @@
+package cache
+
+import (
+	"testing"
+
+	"time"
+)
+
+func TestSimpleCacheRepository(t *testing.T) {
+	cr := NewSimpleCacheRepository(5*time.Minute, 10*time.Minute)
+	if cr == nil {
+		t.Fatal("Failed to create Cache")
+	}
+
+	testData := []int{
+		1, 2, 3,
+	}
+
+	v, ok := cr.Get("foo")
+	if ok {
+		t.Fatal("failed to get empty cache")
+	}
+
+	cr.Set("foo", testData)
+
+	v, ok = cr.Get("foo")
+
+	if len(v.([]int)) != len(testData) {
+		t.Fatal("failed to get cache")
+	}
+
+	for i, d := range testData {
+		if v.([]int)[i] != d {
+			t.Fatal("failed to get cache")
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/otknoy/av_search_be/cache"
 	"github.com/otknoy/av_search_be/search"
 )
 
@@ -11,7 +13,7 @@ var port string = "8080"
 
 func main() {
 	handler := &search.Handler{}
-	handler.Cache = search.NewCache()
+	handler.Cache = cache.NewSimpleCacheRepository(5*time.Minute, 10*time.Minute)
 
 	http.HandleFunc("/search", handler.Search)
 
