9109fc1369dad0c16badd545474173533b3d1ccd
otknoy <otknoy@gmail.com>
2017-11-25 23:48:20 +0900

    rename

--- handler.go
+++ handler.go
@@ -1,4 +1,4 @@
-package search
+package main
 
 import (
 	"encoding/json"
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/otknoy/av_search_be/cache"
+	"github.com/otknoy/av_search_be/search"
 )
 
 type Handler struct {
@@ -26,7 +27,7 @@ func (h *Handler) Search(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response = SearchItems(keyword)
+	response = search.SearchItems(keyword)
 
 	h.Cache.Set(cacheKey, response)
 
--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"time"
 
 	"github.com/otknoy/av_search_be/cache"
-	"github.com/otknoy/av_search_be/search"
 )
 
-var port string = "8080"
+var port = "8080"
 
 func main() {
-	handler := &search.Handler{}
+	handler := &Handler{}
 	handler.Cache = cache.NewSimpleCacheRepository(24*60*time.Minute, 3*24*60*time.Minute)
 
 	http.HandleFunc("/search", handler.Search)
